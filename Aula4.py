a67a2bbb1cc57c7a8b50ea77e18cec34591b2bc8
Pedro Luiz Marcondes <plmsi06@gmail.com>
2025-08-08 14:40:03 -0300

    ajustando headers

--- Aula4.py
+++ Aula4.py
@@ -6,39 +6,38 @@ import plotly.express as px
 # Define o título da página, o ícone e o layout para ocupar a largura inteira.
 st.set_page_config(
     page_title="Dashboard de Salários na Área de Dados",
-    page_icon="📊",
+    page_icon=":bar_chart:",
     layout="wide",
 )
 
 # --- Carregamento dos dados ---
-df = pd.read_csv("https://raw.githubusercontent.com/vqrca/dashboard_salarios_dados/refs/heads/main/dados-imersao-final.csv")
-
+df = pd.read_csv("https://raw.githubusercontent.com/pmarcondes/DashBoardPython/main/dados-imersao-final.csv")
 # --- Barra Lateral (Filtros) ---
 st.sidebar.header("🔍 Filtros")
 
 # Filtro de Ano
-anos_disponiveis = sorted(df['ano'].unique())
+anos_disponiveis = sorted(df['work_year'].unique())
 anos_selecionados = st.sidebar.multiselect("Ano", anos_disponiveis, default=anos_disponiveis)
 
 # Filtro de Senioridade
-senioridades_disponiveis = sorted(df['senioridade'].unique())
+senioridades_disponiveis = sorted(df['experience_level'].unique())
 senioridades_selecionadas = st.sidebar.multiselect("Senioridade", senioridades_disponiveis, default=senioridades_disponiveis)
 
 # Filtro por Tipo de Contrato
-contratos_disponiveis = sorted(df['contrato'].unique())
+contratos_disponiveis = sorted(df['employment_type'].unique())
 contratos_selecionados = st.sidebar.multiselect("Tipo de Contrato", contratos_disponiveis, default=contratos_disponiveis)
 
 # Filtro por Tamanho da Empresa
-tamanhos_disponiveis = sorted(df['tamanho_empresa'].unique())
+tamanhos_disponiveis = sorted(df['company_size'].unique())
 tamanhos_selecionados = st.sidebar.multiselect("Tamanho da Empresa", tamanhos_disponiveis, default=tamanhos_disponiveis)
 
 # --- Filtragem do DataFrame ---
 # O dataframe principal é filtrado com base nas seleções feitas na barra lateral.
 df_filtrado = df[
-    (df['ano'].isin(anos_selecionados)) &
-    (df['senioridade'].isin(senioridades_selecionadas)) &
-    (df['contrato'].isin(contratos_selecionados)) &
-    (df['tamanho_empresa'].isin(tamanhos_selecionados))
+    (df['work_year'].isin(anos_selecionados)) &
+    (df['experience_level'].isin(senioridades_selecionadas)) &
+    (df['employment_type'].isin(contratos_selecionados)) &
+    (df['company_size'].isin(tamanhos_selecionados))
 ]
 
 # --- Conteúdo Principal ---
@@ -49,10 +48,10 @@ st.markdown("Explore os dados salariais na área de dados nos últimos anos. Uti
 st.subheader("Métricas gerais (Salário anual em USD)")
 
 if not df_filtrado.empty:
-    salario_medio = df_filtrado['usd'].mean()
-    salario_maximo = df_filtrado['usd'].max()
+    salario_medio = df_filtrado['salary_in_usd'].mean()
+    salario_maximo = df_filtrado['salary_in_usd'].max()
     total_registros = df_filtrado.shape[0]
-    cargo_mais_frequente = df_filtrado["cargo"].mode()[0]
+    cargo_mais_frequente = df_filtrado["job_title"].mode()[0]
 else:
     salario_medio, salario_mediano, salario_maximo, total_registros, cargo_mais_comum = 0, 0, 0, ""
 
@@ -71,14 +70,14 @@ col_graf1, col_graf2 = st.columns(2)
 
 with col_graf1:
     if not df_filtrado.empty:
-        top_cargos = df_filtrado.groupby('cargo')['usd'].mean().nlargest(10).sort_values(ascending=True).reset_index()
+        top_cargos = df_filtrado.groupby('job_title')['salary_in_usd'].mean().nlargest(10).sort_values(ascending=True).reset_index()
         grafico_cargos = px.bar(
             top_cargos,
-            x='usd',
-            y='cargo',
+            x='salary_in_usd',
+            y='job_title',
             orientation='h',
             title="Top 10 cargos por salário médio",
-            labels={'usd': 'Média salarial anual (USD)', 'cargo': ''}
+            labels={'salary_in_usd': 'Média salarial anual (USD)', "job_title": ''}
         )
         grafico_cargos.update_layout(title_x=0.1, yaxis={'categoryorder':'total ascending'})
         st.plotly_chart(grafico_cargos, use_container_width=True)
@@ -89,10 +88,10 @@ with col_graf2:
     if not df_filtrado.empty:
         grafico_hist = px.histogram(
             df_filtrado,
-            x='usd',
+            x='salary_in_usd',
             nbins=30,
             title="Distribuição de salários anuais",
-            labels={'usd': 'Faixa salarial (USD)', 'count': ''}
+            labels={'salary_in_usd': 'Faixa salarial (USD)', 'count': ''}
         )
         grafico_hist.update_layout(title_x=0.1)
         st.plotly_chart(grafico_hist, use_container_width=True)
@@ -103,7 +102,7 @@ col_graf3, col_graf4 = st.columns(2)
 
 with col_graf3:
     if not df_filtrado.empty:
-        remoto_contagem = df_filtrado['remoto'].value_counts().reset_index()
+        remoto_contagem = df_filtrado['remote_ratio'].value_counts().reset_index()
         remoto_contagem.columns = ['tipo_trabalho', 'quantidade']
         grafico_remoto = px.pie(
             remoto_contagem,
@@ -120,14 +119,14 @@ with col_graf3:
 
 with col_graf4:
     if not df_filtrado.empty:
-        df_ds = df_filtrado[df_filtrado['cargo'] == 'Data Scientist']
-        media_ds_pais = df_ds.groupby('residencia_iso3')['usd'].mean().reset_index()
+        df_ds = df_filtrado[df_filtrado["job_title"] == 'Data Scientist']
+        media_ds_pais = df_ds.groupby('residencia_iso3')['salary_in_usd'].mean().reset_index()
         grafico_paises = px.choropleth(media_ds_pais,
             locations='residencia_iso3',
-            color='usd',
+            color='salary_in_usd',
             color_continuous_scale='rdylgn',
             title='Salário médio de Cientista de Dados por país',
-            labels={'usd': 'Salário médio (USD)', 'residencia_iso3': 'País'})
+            labels={'salary_in_usd': 'Salário médio (USD)', 'residencia_iso3': 'País'})
         grafico_paises.update_layout(title_x=0.1)
         st.plotly_chart(grafico_paises, use_container_width=True)
     else:
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # DashBoardPython
+
+# 1-Criar o ambiente virtual:
+#    python -m venv .venv
+# 2-Ativar o ambiente virtual em Windows:
+#    .venv\Scripts\Activate
+# 3-Ativar o ambiente virtual em MAC/LINUX:
+#    source .venv/bin/activate
+# Criar um arquivo chamado requirements.txt e adicionar os pacotes necessários
+#    pandas==2.2.3
+#    streamlit==1.44.1
+#    plotly==5.24.1
+# 4-Instalar as bibliotecas necessárias
+#    pip install -r requirements.txt
+# 5-Criar a Interface do Dashboard com Streamlit
+
+# Realizar o deploy do Dashboard no Streamlit Cloud: https://streamlit.io/cloud
